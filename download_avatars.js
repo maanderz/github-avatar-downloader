6d6870dd563ae692332ab3c618dd4af16b0651e5
Mandy Fung <mandyfung93@gmail.com>
2018-09-03 23:47:31 +0000

    added a secret

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,22 @@
 var request = require('request');
+var ignore = require('secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-    });
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      'Authorization': 'ignore'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
+
+
+
+
