2a5e4daef30fbbd16acd68766a5436aaf7079fbf
Mandy Fung <mandyfung93@gmail.com>
2018-09-04 01:39:05 +0000

    first callback with json

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var ignore = require('secrets');
+var ignore = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,15 +7,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
       'Authorization': 'ignore'
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    //console.log('JSON', body);
+    cb(err, JSON.parse(body));
   });
 }
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
 
 
 
