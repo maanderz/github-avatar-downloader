65ca4320022245f4628e54a6b8c395d67ca6e972
Mandy Fung <mandyfung93@gmail.com>
2018-09-04 03:30:55 +0000

    linting practice

--- download_avatars.js
+++ download_avatars.js
@@ -6,43 +6,45 @@ var repoName = process.argv[3];
 
 // console.log('Welcome to the GitHub Avatar Downloader!');
 
+// defining which owner and name to retrieve data from github
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = process.argv[2] {
+  var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
       'Authorization': 'ignore'
-    }
-  };
+  }
+};
 
-  request(options, function(err, res, body) {
+//sets up callback parameters
+request(options, function(err, res, body) {
     //console.log('JSON', body);
-    cb(err, JSON.parse(body));
+  cb(err, JSON.parse(body));
   });
 }
-//       console.log("Result:", avaUrl);
-//     }
-//     console.log("Errors:", err);
-// });
+
+//downloads the images - if the image isn't present it will go to the .pipe for a designated file destination
 function downloadImageByURL(avaUrl, filePath) {
   request.get(avaUrl)
     .on('error', function (err) {
       throw err;
-       })
+    })
     .on('response', function (res) {
       console.log('Response Status Message: ', res.statusCode + " " + res.statusMessage);
-      })
+    })
     .pipe(fs.createWriteStream('./' + filePath))
 }
 
-getRepoContributors('jquery', 'jquery', function(err, result) {
-    if (err) {
-      console.log("error:" err);
-      throw err;
+//the callback function
+//loops through the objects to find the avatar_url keys
+//designates the exact filepath for image to download
+getRepoContributors(repoOwner, repoName, function(err, result) {
+    if (!repoOwner || !repoName) {
+      console.log("error: " + err);
     }
     var avaUrl = "";
-    for (var i = 0; i < result.length; i++) {
-      avaUrl = result[i].avatar_url;
-    downloadImageByURL(avaUrl, "./avatar/" + result[i].login + '.jpg');
-  }
+      for (var i = 0; i < result.length; i++) {
+        avaUrl = result[i].avatar_url;
+        downloadImageByURL(avaUrl, "./avatar/" + result[i].login + '.jpg');
+      }
 })
